d6eb64b56af9f36691044585cb06d828e2d2d17b
Dishan Nahitiya <dishan3x@gmail.com>
2020-06-25 03:49:22 -0500

    image construction sucess

--- project_1/app.py
+++ project_1/app.py
@@ -7,7 +7,10 @@ import dash_html_components as html
 import numpy as np
 import onnxruntime as rt
 from PIL import Image
-from skimage import io
+#from skimage import io
+import base64
+from io import BytesIO,StringIO
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -39,17 +42,108 @@ app.layout = html.Div([
 
 def parse_contents(contents, filename, date):
 
-    sess = rt.InferenceSession("dishan_segnet_v2.onnx")
-    #print(sess)
-    #img = io.ImageCollection(contents, conserve_memory=True)
-    
-    #np_img = np.array(img)
-    #print("np_img.shape",np_img.shape)
-    #np_reshape = np.reshape(img,(1, 3, size,size))
-    #floatAstype = np.float32(np_reshape)
-    # Run the model
-    # pred_onx = sess.run("",{input_name:floatAstype})
+    print(contents)
+    content_type, content_string = contents.split(',')
 
+    sess = rt.InferenceSession("dishan_segnet_v2.onnx")
+    print("****************************************")
+    # encode frame 
+    #encoded_string = base64.b64encode(contents.read())
+    # decode frame
+    #decoded_string = base64.b64decode(encoded_string)
+    #decoded_img = np.fromstring(decoded_string, dtype=np.uint8)
+    #decoded_img = decoded_img.reshape(contents.shape)
+    decoded = base64.b64decode(content_string)
+    #print(decoded)
+
+    #im1 = Image.open(decoded)
+    #print(im1)
+    im = Image.open(BytesIO(base64.b64decode(content_string)))
+
+    print(im)
+
+
+    
+    pix = im.load()
+    print("******pix**********************************")
+    print(pix)
+
+    np_img = np.array(im)
+    size = 256
+    print("np_img.shape",np_img.shape)
+    np_reshape = np.reshape(im,(1, 3, size,size))
+    floatAstype = np.float32(np_reshape)
+    input_name = sess.get_inputs()[0].name
+    output_name = sess.get_outputs()[-1].name
+
+    # Run the model
+    pred_onx = sess.run("",{input_name:floatAstype})
+
+    print(pred_onx)
+
+
+    # size is 256
+    width = size
+    height = size
+
+
+    # Creating the array 
+
+    image_construced = np.empty(shape=[width, height])
+    max_probability_holder = np.empty(shape=[width, height])
+    label_holder = np.empty(shape=[width, height])
+
+    for x in range(width):
+        for y in range(height):
+            index = 0
+            max_prediction = 0
+            for i in range(0,4):
+                pred = pred_onx[0][0][i][x][y]
+                if pred > max_prediction:
+                    index = i
+                    max_prediction = pred
+            
+            
+            max_probability_holder[x][y] = max_prediction
+            
+            # ignore the rest of the max_prediction prediction portion
+            
+            # End of for loop for each row of 4 softmax probabilities
+            # Then we check with weights to see which catogory its in 
+            
+            # Blue_per   =   0.3573605344575649
+            # Pink_per   =   0.3228344583030248
+            # Yellow_per =   0.31691428485563417
+            # Bed_per    =  0.0028907223837762435
+        
+            if max_prediction > 0.3573605344575649:
+                image_construced[x][y] = 1
+                label_holder = "canopy"
+            elif max_prediction >  0.3228344583030248:
+                image_construced[x][y] = 2
+                label_holder = "soil"
+            elif max_prediction >  0.31691428485563417:
+                image_construced[x][y] = 3
+                label_holder = "stubble"
+            else:
+                image_construced[x][y] = 4
+                label_holder = "None"
+
+    print(max_probability_holder[0][0])
+    print(max_probability_holder[0][1])
+    print(max_probability_holder[0][2])
+    
+    imgloo = Image.fromarray(image_construced, 'RGB')                  #Crée une image à partir de la matrice
+    buffer = BytesIO()
+    imgloo.save(buffer,format="JPEG")                  #Enregistre l'image dans le buffer
+    myimage = buffer.getvalue() 
+    print("BUfffffffffffffffffffffffffffffffffffffffffffererer")
+    #print(myimage)
+    sd = ""
+    sd = base64.b64encode(myimage)
+    srtt = "data:image/jpeg;base64,"+sd.decode('utf-8')
+    print(srtt)
+    print("here") ;    
     return html.Div([
         html.H5(filename),
         html.H6(datetime.datetime.fromtimestamp(date)),
@@ -60,6 +154,14 @@ def parse_contents(contents, filename, date):
         html.Hr(),
         html.Div('Raw Content'),
         html.Pre(contents[0:200] + '...', style={
+            'whiteSpace': 'pre-wrap',
+            'wordBreak': 'break-all'
+        }),
+        
+        html.Img(src=srtt),
+        html.Hr(),
+        html.Div('Raw Content'),
+        html.Pre(srtt[0:200] + '...', style={
             'whiteSpace': 'pre-wrap',
             'wordBreak': 'break-all'
         })
@@ -81,3 +183,4 @@ def update_output(list_of_contents, list_of_names, list_of_dates):
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+    #app.run_server()
