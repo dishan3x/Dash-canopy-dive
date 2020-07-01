ccf66a6b66c7920cede0cea06f6e177d941f0af3
dishan <dishan3x@gmail.com>
2020-07-01 05:46:10 -0500

    testing bootstrap

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css','dbc.themes
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+
 """ navbar = dbc.NavbarSimple(
     children=[
         dbc.NavItem(dbc.NavLink("Page 1", href="#")),
