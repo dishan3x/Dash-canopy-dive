3108874aff6c3798e6574d7c6afef673fd85c216
dishan <dishan3x@gmail.com>
2020-07-01 23:34:17 -0500

    navigation bar works

--- app.py
+++ app.py
@@ -14,27 +14,14 @@ import plotly.express as px
 import dash_bootstrap_components as dbc
 
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css','dbc.themes.BOOTSTRAP']
+#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css','dbc.themes.BOOTSTRAP']
+external_stylesheets = [dbc.themes.BOOTSTRAP]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 
 navbar = dbc.NavbarSimple(
-    children=[
-        dbc.title('Hey'),
-        #dbc.NavItem(dbc.NavLink("Page 1", href="#")),
-        #dbc.DropdownMenu(
-        #    children=[
-        #        dbc.DropdownMenuItem("More pages", header=True),
-        #        dbc.DropdownMenuItem("Page 2", href="#"),
-        #        dbc.DropdownMenuItem("Page 3", href="#"),
-        nav=True,
-        in_navbar=True,
-        label="More",
-        ),
-    ],
     brand="NavbarSimple",
-    brand_href="#",
     color="primary",
     dark=True,
 ) 
@@ -68,7 +55,7 @@ body = html.Div([
     html.Div(id='output-image-upload'),
 ])
 
-
+# Collecting all components
 app.layout = html.Div([navbar,body])
 
 def parse_contents(contents, filename, date):
