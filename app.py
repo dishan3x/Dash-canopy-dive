0cef81f2cc046e74a75c09c626c7f4df2b170288
dishan <dishan3x@gmail.com>
2020-07-03 18:56:30 -0500

    revamp the image analysis flow

--- app.py
+++ app.py
@@ -52,62 +52,10 @@ body =dbc.Container([
     ),
 
     html.Div(id='output-image-upload'),
-    html.A(
-        id="download-content",
-        download="image.png",
-        children=[
-            dbc.Button(
-                "Download image",
-                id="down-load_button",
-                color="primary",
-                #children=html.Img(
-                #    src="assets/some_image.svg",
-                #    className="button-image",
-                #),
-                className="inline_button",
-            )
-        ],
-    )
 ])
 
 # Collecting all components
 app.layout = html.Div([navbar,body])
-
-def parse_contents(contents, filename, date, contructed_image):
-
-    returnDiv = html.Div([
-        dbc.Row(
-            [
-            #html.H5(filename),
-            #html.H6(datetime.datetime.fromtimestamp(date)),
-
-            # HTML images accept base64 encoded strings in the same format
-            # that is supplied by the upload
-            dbc.Col
-            (
-                html.Div([
-                    html.H5("Original image"),
-                    html.Img(id="original_image",src=contents)
-                    ])
-            ),
-            
-            dbc.Col
-            (    
-                html.Div([  
-                    html.H5("Constructed Image"),
-                    html.Img(id="constructed_img",src=contructed_image)
-                ])
-
-            ),
-
-            ]),
-
-
-      
-    ])
-    
-
-    return returnDiv
 
 def analyse_image_fun(contents, filename, date):
 
@@ -139,38 +87,33 @@ def analyse_image_fun(contents, filename, date):
 
     # Choosing class of the highest index 
     # highest probability of each pixel cell 
-    highest_index = np.argmax(pred_onx[0][0], axis=0)
+    highest_probability_index = np.argmax(pred_onx[0][0], axis=0)
     
 
     for x in range(size):
         for y in range(size):
-            index = highest_index[x][y]
+            index = highest_probability_index[x][y]
             if index == 0:
 
-                # canopy
+                # canopy -> green
                 rgb_array[x,y,0] = 0
                 rgb_array[x,y,1] = 255
                 rgb_array[x,y,2] = 0
 
             elif index == 1:
 
-                # soil
+                # soil -> brown
                 rgb_array[x,y,0] = 165
                 rgb_array[x,y,1] = 42
                 rgb_array[x,y,2] = 42
 
             elif index == 2:
 
-                #stubble 
+                #stubble  -> blue
                 rgb_array[x,y,0] = 0
                 rgb_array[x,y,1] = 0
                 rgb_array[x,y,2] = 255
-            else:
- 
-                #None
-                rgb_array[x,y,0] = 255
-                rgb_array[x,y,1] = 0
-                rgb_array[x,y,2] = 0
+
 
 
     pil_img = Image.fromarray(rgb_array)
@@ -178,19 +121,112 @@ def analyse_image_fun(contents, filename, date):
     pil_img.save(buff, format="JPEG")
     new_image_string = base64.b64encode(buff.getvalue()).decode("utf-8")
     new_image_string = "data:image/JPEG;base64,"+new_image_string
+
+
+    # Getting the perncentage value 
+    unique, counts = np.unique(highest_probability_index, return_counts=True)
+    pixel_spread = dict(zip(unique, counts))
+
+    print(pixel_spread)
+    print(sum(counts))
+    sum_pixel           = sum(counts) 
+    canopy_percentage   = pixel_spread[0]/sum_pixel
+    soil_percentage     = pixel_spread[1]/sum_pixel
+    stubble_percentage  = pixel_spread[2]/sum_pixel
+
+    pixel_count_data = {
+        'canopy_p':canopy_percentage,
+        'soil_p':soil_percentage,
+        'stubble_p':stubble_percentage
+        }
+
+    #img_html_div_constructed = [
+    #           analysed_info_tohtml_func(c, n, d, i) for c, n, d, i in
+    #            zip(list_of_contents, list_of_names, list_of_dates,re_created_string)
+    #        ] 
+
+    img_html_div_constructed = analysed_info_tohtml_func(contents, filename, date,new_image_string,pixel_count_data)
+
+
     
+    #analysed_information = {
+    #'constructed_image':new_image_string,
+    #'percentages':pixel_count_data
+    #}
+
+    return img_html_div_constructed
+
+def analysed_info_tohtml_func(contents, filename, date, contructed_image,pixel_count_data):
+
+
+    table_header = [
+        html.Thead(html.Tr([html.Th("Type"), html.Th("Percentage")]))
+    ]
+
+    row1 = html.Tr([html.Td("Canopy"), html.Td(pixel_count_data['canopy_p'])])
+    row2 = html.Tr([html.Td("Soil"), html.Td(pixel_count_data['soil_p'])])
+    row3 = html.Tr([html.Td("Stubble"), html.Td(pixel_count_data['stubble_p'])])
+ 
+
+    table_body = [html.Tbody([row1, row2, row3])]
+
+    table = [dbc.Table(table_header + table_body, bordered=True)]
+
+    download_button = html.A(
+            id="download-content",
+            download="image.png",
+            href = contructed_image,
+            children=[
+            dbc.Button(
+                "Download image",
+                id="down-load_button",
+                color="primary",
+
+                #children=html.Img(
+                #    src="assets/some_image.svg",
+                #    className="button-image",
+                #),
+                className="inline_button",
+            )
+            ])
+
+    returnDiv = html.Div([
+        dbc.Row(
+            [
+            #html.H5(filename),
+            #html.H6(datetime.datetime.fromtimestamp(date)),
+
+            # HTML images accept base64 encoded strings in the same format
+            # that is supplied by the upload
+            dbc.Col
+            (
+                html.Div([
+                    html.H5("Original image"),
+                    html.Img(id="original_image",src=contents)
+                    ])
+            ),
+            
+            dbc.Col
+            (    
+                html.Div([  
+                    html.H5("Constructed Image"),
+                    html.Img(id="constructed_img",src=contructed_image)
+                ])
+
+            ),
+
+            ]),
+            #table,
+            download_button,
+    ])
     
 
-    return new_image_string
-
+    return returnDiv    
     
+      
 
 # call bacl for upload input 
-@app.callback(
-    [
-        Output(component_id='output-image-upload',component_property='children'),
-        Output(component_id="download-content",component_property="href"),
-    ],
+@app.callback(Output(component_id='output-image-upload',component_property='children'),
     [Input(component_id='upload-image', component_property= 'contents')],
     [State('upload-image', 'filename'),
     State('upload-image', 'last_modified')])
@@ -198,24 +234,24 @@ def update_output(list_of_contents, list_of_names, list_of_dates):
     if list_of_contents is not None:
         # children will return a html content
         #children = [
-        #    parse_contents(c, n, d) for c, n, d in
+        #    analysed_info_tohtml_func(c, n, d) for c, n, d in
         #    zip(list_of_contents, list_of_names, list_of_dates)]  # assigning c,n,d as  inputs and zip version of inputs
         
         
-        image_contructed = [
+        analysed_information = [
             analyse_image_fun(c, n, d) for c, n, d in
             zip(list_of_contents, list_of_names, list_of_dates)]
-        
-        # Need to pass all the information in creating the divs
-        img_html_div_constructed = [
-            parse_contents(c, n, d, i) for c, n, d, i in
-            zip(list_of_contents, list_of_names, list_of_dates,image_contructed )]  # assigning c,n,d as  inputs and zip version of inputs
 
-        #print(children)
-        #print("$$$$$$$$$$$$$$",len(return_values))
-        return img_html_div_constructed,image_contructed[0]
+        #re_created_string = analysed_information[0]['constructed_image']
+        # Need to pass all the information in creating the divs
+        #img_html_div_constructed = [
+        #        analysed_info_tohtml_func(c, n, d, i) for c, n, d, i in
+        #        zip(list_of_contents, list_of_names, list_of_dates,re_created_string)
+        #    ]  # assigning c,n,d as  inputs and zip version of inputs
+
+        return analysed_information
     else:
-        return '','' # Place holder the when the call back is called in preload
+        return '' # Place holder the when the call back is called in preload
 
 
 
